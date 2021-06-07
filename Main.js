68b0f9a1a2ed95127a26358c270b0266eff1c673
Zulkharnain4510 <71696418+Zulkharnain4510@users.noreply.github.com>
2021-06-07 18:25:20 +0530

    Add files via upload

---
+++ Main.js
@@ -0,0 +1,46 @@
+noseX=0;
+noseY=0;
+
+function preload() {
+clown_nose = loadImage('https://postimg.cc/2b7XJkMW.png');
+}
+
+function setup() {
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoad);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoad() {
+console.log('PoseNet Is Initialized');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = result[0].pose.nose.x-15;
+        noseY = result[0].pose.nose.y-15;
+        console.log("nose x =" + noseX);
+        console.log("nose y =" + noseY);
+    }
+}
+
+function draw() {
+    image(video,0, 0, 300, 300);
+    //image(clown_nose, noseX, noseY, 30, 30);
+    fill(255,0,0);
+    stroke(255,0,0);
+    circle(noseX, noseY, 20);
+}
+
+
+function take_snapshot() {
+    save('myFilterImage.png');
+} 
